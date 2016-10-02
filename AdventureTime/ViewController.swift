7ae516c6a7740764cc05cc4417873796876bd8f1
henrykchan <chan.henryk@gmail.com>
2016-10-02 14:23:44 -0400

    Adventure Time 💯

--- AdventureTime/ViewController.swift
+++ AdventureTime/ViewController.swift
@@ -10,16 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var occupationLabel: UILabel!
+    
+    @IBOutlet weak var speciesLabel: UILabel!
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+    
+    var finn: Character!
+    var jake: Character!
+    var bubblegum: Character!
+    var bmo: Character!
+    var lemongrab: Character!
+    var lsp: Character!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createAllCharacters()
+        
+        
     }
-
-  
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        
+        switch sender.titleLabel!.text! {
+        case "Finn":
+            updateViews(with: finn)
+        case "Jake":
+            updateViews(with: jake)
+        case "Bubblegum":
+            updateViews(with: bubblegum)
+        case "BMO":
+            updateViews(with: bmo)
+        case "Lemongrab":
+            updateViews(with: lemongrab)
+        case "LSP":
+            updateViews(with: lsp)
+        default:
+            print("error")
+        }
+    }
     
     
-   
-
+    
+    func createAllCharacters () {
+        
+        finn = Character(name: "Finn The Human", species: "Human", occupation: "Hero", powerLevel: 50.0)
+        jake = Character(name: "Jake The Dog", species: "Penguin", occupation: "Fisher", powerLevel: 10.0)
+        bubblegum = Character(name: "Princess Bubblegum", species: "Princess", occupation: "Princess", powerLevel: 75.0)
+        bmo = Character(name: "BMO", species: "unicorn", occupation: "pilot", powerLevel: 44.0)
+        lemongrab = Character(name: "Lemongrab", species: "tree", occupation: "police", powerLevel: 100.0)
+        lsp = Character (name: "Lumpy Space Princess", species: "princess", occupation: "princess", powerLevel: 22.0)
+    }
+    
+    
+    func updateViews (with character:Character) {
+        
+        characterImageView.image = character.displayImage()
+        nameLabel.text = character.name
+        occupationLabel.text = character.occupation
+        speciesLabel.text = character.species
+        
+        
+        }
+    
+    
 }
 
